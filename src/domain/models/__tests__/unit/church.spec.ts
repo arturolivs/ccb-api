38b81e80fcef13027c62a6a228cb4e8e54eaccbd
artur <artur.oliveirasantiago@gmail.com>
2023-09-25 19:39:15 -0300

    test(church): Add entity and church tests

---
+++ src/domain/models/__tests__/unit/church.spec.ts
@@ -0,0 +1,25 @@
+import { Church, ChurchProps } from '../../church.entity'
+import { ChurchDataBuilder } from '../../testing/helpers/church-data-builder'
+
+describe('Church unit tests', () => {
+  let props: ChurchProps
+  let sut: Church
+
+  beforeEach(() => {
+    props = ChurchDataBuilder({})
+    sut = new Church(props)
+  })
+
+  it('should create a church on constructor method', () => {
+    expect(sut.props.cd_address).toEqual(props.cd_address)
+    expect(sut.props.locale).toEqual(props.locale)
+  })
+
+  it.skip('should validate a church on constructor method', () => {
+    const createInvalidChurch = () => new Church({ cd_address: '', locale: '' })
+    createInvalidChurch()
+    // expect(() => {
+    //   createInvalidChurch()
+    // }).toThrowError(EntityValidationError)
+  })
+})
---
+++ src/domain/models/__tests__/unit/entity.spec.ts
@@ -0,0 +1,48 @@
+import { validate as uuidValidate } from 'uuid'
+import { Entity } from '../../entity'
+import { faker } from '@faker-js/faker'
+
+type StubProps = {
+  prop1: string
+  prop2: number
+}
+
+class StubEntity extends Entity<StubProps> {}
+
+const generateProps = () => ({
+  prop1: faker.lorem.word(),
+  prop2: faker.number.int(),
+})
+
+describe('Entity unit tests', () => {
+  it('should set attributes and props in the constructor', () => {
+    const props = generateProps()
+    const entity = new StubEntity(props)
+
+    expect(entity.props).toStrictEqual(props)
+    expect(entity.createdAt).toBeInstanceOf(Date)
+    expect(entity.id).not.toBeNull()
+    expect(uuidValidate(entity.id)).toBeTruthy()
+  })
+
+  it('should accept a valid uuid', () => {
+    const props = generateProps()
+    const id = faker.string.uuid()
+    const entity = new StubEntity(props, id)
+
+    expect(uuidValidate(entity.id)).toBeTruthy()
+    expect(entity.id).toBe(id)
+  })
+
+  it('should convert a entity to a Javascript Object', () => {
+    const props = generateProps()
+    const id = faker.string.uuid()
+    const entity = new StubEntity(props, id)
+
+    expect(entity.toJSON()).toStrictEqual({
+      id,
+      createdAt: entity.createdAt?.toISOString(),
+      ...props,
+    })
+  })
+})
