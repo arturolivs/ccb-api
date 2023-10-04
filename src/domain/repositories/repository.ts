ce515ffdda8d72c982f1ed4eec38750e2dcdbc71
artur <artur.oliveirasantiago@gmail.com>
2023-10-04 17:10:23 -0300

    chore: Created mock repository

--- src/domain/repositories/repository.ts
+++ src/domain/repositories/repository.ts
@@ -1,7 +1,7 @@
 import { Entity } from '../models/entity'
 
 export interface Repository<E extends Entity> {
-  create(entity: E): Promise<void>
+  create(entity: E): Promise<E>
   byId(id: string): Promise<E>
   findAll(): Promise<E[]>
   update(entity: E): Promise<void>
---
+++ src/domain/testing/helpers/repository.mock.ts
@@ -0,0 +1,10 @@
+import { Entity } from '@/domain/models/entity'
+import { Repository } from '@/domain/repositories/repository'
+
+export const createMockRepository = <T extends Entity>(): Repository<T> => ({
+  create: jest.fn(),
+  byId: jest.fn(),
+  findAll: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+})
