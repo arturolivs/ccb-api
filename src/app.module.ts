9c944473f0cc5a88d6306b27bf7336b360cb4719
artur <artur.oliveirasantiago@gmail.com>
2023-10-06 16:35:08 -0300

    chore: Add ChurchesModule on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { DatabaseModule } from '@infra/database/database.module'
-import { ConfigModule } from '@nestjs/config'
-
+import { ChurchesModule } from '@interfaces/modules/churches.module'
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [`.env.${process.env.NODE_ENV}`],
     }),
+    ChurchesModule,
     DatabaseModule,
   ],
   controllers: [AppController],
